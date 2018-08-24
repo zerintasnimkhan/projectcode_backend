84ee9aa26b2fee40b29245259188aa3df22c9585
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2018-08-24 13:30:13 +0200

    upgrade to v2.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ const fs = require('fs');
 const del = require('del');
 const $ = require('gulp-load-plugins')();
 
-// Utility to ignore Node modules and Bower components
+// Utility to ignore unnecessary files
 // when generating the glob patterns array for gulp.src()
 function addDefSrcIgnore (srcArr) {
   return srcArr.concat([
     '!**/REMOVE{,/**}',
     '!node_modules{,/**}',
-    '!client/bower_components{,/**}',
     '!private{,/**}',
     '!dist{,/**}',
     '!.git{,/**}',
@@ -68,7 +67,7 @@ gulp.task('dist', ['remove-solutions'], function () {
   const npmConfig = require('./package.json');
   npmConfig.name = removeMaster(npmConfig.name);
   npmConfig.repository.url = removeMaster(npmConfig.repository.url);
-  npmConfig.config.ghooks['pre-commit'] = 'gulp lint';
+  npmConfig.scripts['precommit'] = 'gulp lint';
   fs.writeFileSync('dist/package.json', JSON.stringify(npmConfig, null, 2));
 
   const esLintConfig = require('./.eslintrc.json');
